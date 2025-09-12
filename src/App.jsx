6a2bbaa6f3f7c6c265dd6ff5a2802a66be704603
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2025-09-12 19:02:52 -0300

    feat: console log print item value

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [item, setItem] = useState("")
+
+  function addBtn() {
+    console.log(item)
+  }
+
+
+  return (
+    <div>
+      <h1>Todo-List</h1>
+      <div>
+        <table id='todo'>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Status</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th><input type="text" value={ item } onChange={ (e) => setItem(e.target.value) } placeholder='Novo item' name='item'/></th>
+              <th><input type="checkbox" name="status" disabled/></th>
+              <th><input type="button" value="adicionar" name='adicionar' onClick={addBtn}/></th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  )
+}
+
+
+
+export default App

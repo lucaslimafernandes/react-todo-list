8b41e4597631e64912beb00e2270cad7c006d7ac
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2025-09-12 19:11:26 -0300

    feat: add item list

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ import './App.css'
 function App() {
 
   const [item, setItem] = useState("")
+  const [todos, setTodos] = useState([])
 
   function addBtn() {
     console.log(item)
+    if (item.trim() == "") { return }
+
+    setTodos([...todos, { text: item, done: false}])
+    setItem("")
+
   }
 
 
@@ -30,6 +36,13 @@ function App() {
               <th><input type="checkbox" name="status" disabled/></th>
               <th><input type="button" value="adicionar" name='adicionar' onClick={addBtn}/></th>
             </tr>
+            { todos.map((todo, index) => (
+              <tr>
+                <td>{ todo.text }</td>
+                <td><input type="checkbox" name="done" id="" checked={ todo.done } /> </td>
+                <td><input type="button" value="excluir" /></td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>

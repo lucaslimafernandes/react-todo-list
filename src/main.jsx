915797e6c11c268a3035dff8d77068ea47f23d3e
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2025-09-12 22:12:09 -0300

    feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+      <Route path='/list/:id' element={<App />}></Route>
+      <Route path='*' element={<Navigate to="/" replace />}></Route>
+    </Routes>
+  </BrowserRouter>
 )

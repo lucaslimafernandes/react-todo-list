db37058c14eb42945a0b925d7257dc8aa4704764
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2025-09-12 22:54:43 -0300

    feat: page list to share

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,15 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useNavigate } from "react-router-dom";
 
-function App() {
-
-  const [item, setItem] = useState("")
-  const [todos, setTodos] = useState(() => {
-    const saved = localStorage.getItem("meus-todos")
-    return saved ? JSON.parse(saved) : []
-  })
-
-  useEffect(() => {
-    localStorage.setItem("meus-todos", JSON.stringify(todos))
-  }, [todos])
-
-  function addBtn() {
-    if (item.trim() == "") { return }
-
-    setTodos([...todos, { text: item, done: false}])
-    setItem("")
-
+export default function App() {
+  const navigate = useNavigate();
+  function newList() {
+    const id = crypto.randomUUID();
+    navigate(`/list/${id}`);
   }
-
-  function changeStatus(index) {
-
-    setTodos(
-      todos.map((todo, i) =>
-      i === index ? { ...todo, done: !todo.done } : todo
-      )
-    )
-
-  }
-
-  function deleteItem(index) {
-    setTodos(todos.filter((_, i) => i !== index))
-  }
-
-  function clearStorage() {
-    localStorage.removeItem("meus-todos")
-    setTodos([])
-  }
-
-
   return (
     <div>
       <h1>Todo-List</h1>
-      <div>
-        <table id='todo'>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Status</th>
-              <th> </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th><input type="text" value={ item } onChange={ (e) => setItem(e.target.value) } placeholder='Novo item' name='item'/></th>
-              <th><input type="checkbox" name="status" disabled/></th>
-              <th><input type="button" value="adicionar" name='adicionar' onClick={addBtn}/></th>
-            </tr>
-            { todos.map((todo, index) => (
-              <tr key={index}>
-                <td>{ todo.text }</td>
-                <td><input type="checkbox" name="done" id="" checked={ todo.done } onChange={() => changeStatus(index)}/> </td>
-                <td><input type="button" value="excluir" onClick={() => deleteItem(index)}/></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div>
-        <input type="button" value="clear all" onClick={() => clearStorage()}/>
-      </div>
+      <button onClick={newList}>Criar nova lista</button>
     </div>
-
-  )
+  );
 }
-
-
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ListPage from './ListPage.jsx';
 
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
@@ -14,7 +15,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />}></Route>
-      <Route path='/list/:id' element={<App />}></Route>
+      <Route path='/list/:id' element={<ListPage />}></Route>
       <Route path='*' element={<Navigate to="/" replace />}></Route>
     </Routes>
   </BrowserRouter>
